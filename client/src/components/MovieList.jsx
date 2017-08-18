eaec27b8da3d0e28b2c1ca80d1991f868dd275d1
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-18 12:32:42 -0400

    update movie list

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Movie from './Movie';
+import MovieAddForm from './MovieAddForm';
+import MovieEditForm from './MovieEditForm';
 
 const MoviesList = (props) => {
     return (
         <div className="movies-list">
+            <MovieAddForm handleMovieSubmit={props.handleMovieSubmit} />
             {props.movieData.map(movie => {
-                return <Movie movie={movie} key={movie.id} />
+               if (props.currentMovieId === movie.id) {
+                   return <MovieEditForm key={movie.id} movie={movie} handleMovieSubmit={props.handleMovieEditSubmit} />
+               } else return <Movie movie={movie} selectEditedMovie={props.selectEditedMovie} key={movie.id} />
             })}
             </div>
     )
