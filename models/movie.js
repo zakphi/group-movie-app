bc4dd174eb239613640520a9bcee3c4248b9156b
Philip Zak <zakphi@gmail.com>
2017-08-18 11:57:47 -0400

    create more movie models

--- models/movie.js
+++ models/movie.js
@@ -6,4 +6,38 @@ Movie.findAll() = () => {
   return db.query('SELECT * FROM movies ORDER BY id ASC')
 }
 
+Movie.findById = (id) => {
+  return db.oneOrNone(`
+    SELECT * FROM movies
+    WHERE id = $1
+  `, [id])
+}
+
+Movie.create = (movie) => {
+  return db.one(`
+    INSERT INTO movies
+    (title, description, genre)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `, [movie.title, movie.description, movie.genre])
+}
+
+Movie.update = (movie, id) => {
+  return db.one(`
+    UPDATE movies SET
+    title = $1,
+    description = $2,
+    genre = $3,
+    WHERE id = $4
+    RETURNING *
+  `, [movie.title, movie.description, movie.genre, id])
+}
+
+Movie.destroy = (id) => {
+  return db.none(`
+    DELETE FROM movies
+    WHERE id = $1
+  `, [id])
+}
+
 module.exports = Movie
