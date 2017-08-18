5aa12bacb3f65dd9447c949f7ff202901b8d7d8f
Philip Zak <zakphi@gmail.com>
2017-08-18 10:47:26 -0400

    set up the middleware

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ const passport = require('passport')
 
 const app = express()
 require('dotenv').config()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cookieParser())
+app.use(
+  session({
+    key: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.static('public'))
