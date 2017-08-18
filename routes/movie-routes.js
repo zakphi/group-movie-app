3636824e52cde069e7dabec8db5f1f7858c05984
Philip Zak <zakphi@gmail.com>
2017-08-18 12:09:00 -0400

    add more movie routes

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -4,5 +4,10 @@ const movieRoutes = express.Router()
 const moviesController = require('../controllers/movies-controller')
 
 movieRoutes.get('/', moviesController.index)
+movieRoutes.post('/', moviesController.create)
+
+movieRoutes.get('/:id', moviesController.show)
+movieRoutes.put('/:id', moviesController.update)
+movieRoutes.delete('/:id', moviesController.delete)
 
 module.exports = movieRoutes
