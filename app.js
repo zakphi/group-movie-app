d85b67e189afc786c34cee04a1d04ba82c7fa063
Philip Zak <zakphi@gmail.com>
2017-08-18 10:45:05 -0400

    import dependencies

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const logger = require('morgan')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
