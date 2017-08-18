21217ab4f8aad9ca8cc0edcc7b9cc406a82c316f
Philip Zak <zakphi@gmail.com>
2017-08-18 12:06:01 -0400

    add more movie controllers

--- controllers/movies-controller.js
+++ controllers/movies-controller.js
@@ -16,4 +16,67 @@ moviesController.index = (req, res) => {
     })
 }
 
+moviesController.show = (req, res) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.json({
+        message: 'ok',
+        data: movie
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+}
+
+moviesController.create = (req, res) => {
+  Movie.create({
+    title: req.body.title,
+    description: req.body.description,
+    genre: req.body.genre
+  })
+    .then(movie => {
+      res.json({
+        message: 'movie added successfully',
+        data: movie
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+}
+
+moviesController.update = (req, res) => {
+  Movie.update({
+    title: req.body.title,
+    description: req.body.description,
+    genre: req.body.genre
+  }, req.params.id)
+    .then(movie => {
+      res.json({
+        message: 'movie updated successfully',
+        data: movie
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+}
+
+moviesController.delete = (req, res) => {
+  Movie.destroy(req.params.id)
+    .then(() => {
+      res.json({
+        message: 'movie delete successfully'
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+}
+
 module.exports = moviesController
