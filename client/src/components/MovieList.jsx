fc5588f19cd634c4aff89ee6f197a514fd85b9e8
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-20 16:49:18 -0400

    add div 'moviespage'

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -6,14 +6,16 @@ import MovieEditForm from './MovieEditForm';
 
 const MoviesList = (props) => {
     return (
-        <div className="movies-list">
+        <div className="moviespage">
             <MovieAddForm handleMovieSubmit={props.handleMovieSubmit} />
+            <div className="movies-list">
             {props.movieData.map(movie => {
                if (props.currentMovieId === movie.id) {
                    return <MovieEditForm key={movie.id} movie={movie} handleMovieSubmit={props.handleMovieEditSubmit} />
                } else return <Movie movie={movie} selectEditedMovie={props.selectEditedMovie} key={movie.id} feature={props.feature} featureMovie={props.featureMovie} />
             })}
             </div>
+        </div>
     )
 }
 
