1bf2fb524bfe941a986230877f261e33f44b13fa
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-20 17:59:54 -0400

    conditional render edit on user login

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Movie from './Movie';
 import MovieAddForm from './MovieAddForm';
 import MovieEditForm from './MovieEditForm';
 
-const MoviesList = (props) => {
+class MoviesList extends Component {
+    constructor() {
+        super();
+
+        this.checkLogIn = this.checkLogIn.bind(this);
+    }
+
+    checkLogIn (){
+                if (!this.props.userState) {
+                return '';
+            } else {
+               return (<MovieAddForm handleMovieSubmit={this.props.handleMovieSubmit} />);
+            }
+           }
+
+    render (){
     return (
+
         <div className="moviespage">
-            <MovieAddForm handleMovieSubmit={props.handleMovieSubmit} />
+            {this.checkLogIn()}
             <div className="movies-list">
             {props.movieData.map(movie => {
                if (props.currentMovieId === movie.id) {
                    return <MovieEditForm key={movie.id} movie={movie} handleMovieSubmit={props.handleMovieEditSubmit} />
                } else return <Movie movie={movie} selectEditedMovie={props.selectEditedMovie} key={movie.id} feature={props.feature} featureMovie={props.featureMovie} />
+
             })}
             </div>
         </div>
     )
+    }
 }
 
 export default MoviesList;
