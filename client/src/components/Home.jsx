821ef0bec39d04ec54c9ecfc9f02708937c189f3
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-20 16:45:14 -0400

    change h1

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="home">
-            <h1>Here is the home page</h1>
+            <h1>Welcome to Our Movie App</h1>
         </div>
     )
 }
