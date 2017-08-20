daa9cb93e07d690f4935deaa001f95d512fa40ae
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-20 18:44:07 -0400

    add this.

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -17,21 +17,21 @@ editCheck (){
 render() {
   return (
     <div 
-      className={`movie ${props.feature === props.movie.id
+      className={`movie ${this.props.feature === this.props.movie.id
         ? 'feature' 
         : ''}`}
     >
-        <h3>{props.movie.title}</h3>
-        <p>{props.movie.description}</p>
-        <p>Genre: {props.movie.genre}</p>
-        <span className="edit" onClick={() => props.selectEditedMovie(props.movie.id)}>Edit</span>
+        <h3>{this.props.movie.title}</h3>
+        <p>{this.props.movie.description}</p>
+        <p>Genre: {this.props.movie.genre}</p>
+        {this.editCheck()}
         <div className="featureme">
         {/*<i 
-          onClick={() => props.featureMovie(props.movie.id)}
+          onClick={() => this.props.featureMovie(this.props.movie.id)}
           className="fa fa-star fa-2x" 
         />*/}
-        <button onClick={() => props.featureMovie(props.movie.id)}>favorite</button>
-        {this.editCheck()}
+        <button onClick={() => this.props.featureMovie(this.props.movie.id)}>favorite</button>
+        
       </div>
     </div>
   )
