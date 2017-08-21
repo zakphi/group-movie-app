009f5d81349bfb2862e5fdb7a4eeaf578c96a055
Philip Zak <zakphi@gmail.com>
2017-08-20 21:38:54 -0400

    fix format

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
 
 class Movie extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.editCheck = this.editCheck.bind(this);
   }
 
-editCheck (){
-                if (!this.props.userState) {
-                return '';
-            } else {
-               return (<span className="edit" onClick={() => this.props.selectEditedMovie(this.props.movie.id)}>Edit</span>);
-            }
-           }
-render() {
-  return (
-    <div 
-      className={`movie ${this.props.feature === this.props.movie.id
-        ? 'feature' 
-        : ''}`}
-    >
+  editCheck (){
+    if (!this.props.userState) {
+      return '';
+    } else {
+      return (<span className="edit" onClick={() => this.props.selectEditedMovie(this.props.movie.id)}>Edit</span>);
+    }
+  }
+
+  render() {
+    return (
+      <div className={`movie ${this.props.feature === this.props.movie.id ? 'feature' : ''}`}>
         <h3>{this.props.movie.title}</h3>
         <p>{this.props.movie.description}</p>
         <p>Genre: {this.props.movie.genre}</p>
         {this.editCheck()}
         <div className="featureme">
-        {/*<i 
-          onClick={() => this.props.featureMovie(this.props.movie.id)}
-          className="fa fa-star fa-2x" 
-        />*/}
-        <button onClick={() => this.props.featureMovie(this.props.movie.id)}>favorite</button>
-        
+          <button onClick={() => this.props.featureMovie(this.props.movie.id)}>favorite</button>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default Movie;
--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -5,36 +5,34 @@ import MovieAddForm from './MovieAddForm';
 import MovieEditForm from './MovieEditForm';
 
 class MoviesList extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.checkLogIn = this.checkLogIn.bind(this);
+    this.checkLogIn = this.checkLogIn.bind(this);
+  }
+
+  checkLogIn (){
+    if (!this.props.userState) {
+      return '';
+    } else {
+        return (<MovieAddForm handleMovieSubmit={this.props.handleMovieSubmit} />);
+      }
     }
-
-    checkLogIn (){
-                if (!this.props.userState) {
-                return '';
-            } else {
-               return (<MovieAddForm handleMovieSubmit={this.props.handleMovieSubmit} />);
-            }
-           }
 
     render (){
-    return (
-
+      return (
         <div className="moviespage">
-            {this.checkLogIn()}
-            <div className="movies-list">
+          {this.checkLogIn()}
+          <div className="movies-list">
             {this.props.movieData.map(movie => {
-               if (this.props.currentMovieId === movie.id) {
-                   return <MovieEditForm key={movie.id} movie={movie} handleMovieSubmit={this.props.handleMovieEditSubmit} />
-               } else return <Movie movie={movie} selectEditedMovie={this.props.selectEditedMovie} key={movie.id} feature={this.props.feature} featureMovie={this.props.featureMovie} userState={this.props.userState} />
-
-            })}
-            </div>
+              if (this.props.currentMovieId === movie.id) {
+                return <MovieEditForm key={movie.id} movie={movie} handleMovieSubmit={this.props.handleMovieEditSubmit} />
+              } else return <Movie movie={movie} selectEditedMovie={this.props.selectEditedMovie} key={movie.id} feature={this.props.feature} featureMovie={this.props.featureMovie} userState={this.props.userState} />
+              })}
+          </div>
         </div>
-    )
-    }
+      )
+  }
 }
 
 export default MoviesList;
