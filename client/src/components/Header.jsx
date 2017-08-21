599bf2824403a26935bbf564a5298bc6fbf09280
Philip Zak <zakphi@gmail.com>
2017-08-20 21:37:01 -0400

    conditional rendering

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Header = (props) => {
     return (
-        <header>
-            <div className="logo">Awesome Movie App</div>
-            <nav>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/movies'>Movies</Link></li>
-                <li><Link to='/login'>Log In</Link></li>
-                <li><Link to='/register'>Register</Link></li>
-                <li>User Dashboard</li>
-                <li onClick={props.logOut}><Link to='/'>Log Out</Link></li>
-            </nav>
-        </header>
+      <header>
+          <div className="logo">Awesome Movie App</div>
+          <nav>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/movies'>Movies</Link></li>
+            {!props.userState ? <li><Link to='/register'>Register</Link></li> : ''}
+            {!props.userState ? <li><Link to='/login'>Log In</Link></li> : ''}
+            {props.userState ? <li>User Dashboard</li> : ''}
+            {props.userState ? <li onClick={props.logOut}><Link to='/' replace>Log Out</Link></li> : ''}
+          </nav>
+      </header>
     )
-}
+  }
 
 export default Header;
